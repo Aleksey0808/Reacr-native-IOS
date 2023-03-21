1f487bb652d59550b1d6c3d60fa9acdaa2053d94
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2023-03-21 16:03:46 +0200

    210323

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import * as Font from 'expo-font';
+import Form from "./assets/Screens/RegistrationScreen";
+
+export default function App() {
+  
+  return (
+    <Form />
+  )
+}
